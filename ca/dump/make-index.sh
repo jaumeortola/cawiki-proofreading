a39dc7170d23ad9b4bb2ae8eeba6232132cb122b
jaumeortola <jaumeortola@gmail.com>
2016-02-18 23:04:39 +0000

    adjust folders

--- ca/dump/make-index.sh
+++ ca/dump/make-index.sh
@@ -1,3 +1,3 @@
 #|/bin/bash
-ltwiki_java_dir=/mnt/mydata/github/languagetool/languagetool-wikipedia/target/LanguageTool-wikipedia-3.3-SNAPSHOT/LanguageTool-wikipedia-3.3-SNAPSHOT
+ltwiki_java_dir=~/languagetool/languagetool-wikipedia/target/LanguageTool-wikipedia-3.3-SNAPSHOT/LanguageTool-wikipedia-3.3-SNAPSHOT
 java -jar ${ltwiki_java_dir}/languagetool-wikipedia.jar index-data cawiki-latest-pages-articles.xml index-dir ca 0
