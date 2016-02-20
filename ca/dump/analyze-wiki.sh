6668cbbc422fce43528802aba3189e1a0cdc408b
jaumeortola <jaumeortola@gmail.com>
2016-02-20 08:42:34 +0000

    minor updates

--- ca/dump/analyze-wiki.sh
+++ ca/dump/analyze-wiki.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 ltwiki_java_dir=~/languagetool/languagetool-wikipedia/target/LanguageTool-wikipedia-3.3-SNAPSHOT/LanguageTool-wikipedia-3.3-SNAPSHOT
-java -jar ${ltwiki_java_dir}/languagetool-wikipedia.jar check-data --file cawiki-latest-pages-articles.xml --language ca > analysis-results.txt &
+java -jar ${ltwiki_java_dir}/languagetool-wikipedia.jar check-data --file cawiki-latest-pages-articles.xml --language ca > analysis-results.txt & 2>analysis-errors.txt &
+
