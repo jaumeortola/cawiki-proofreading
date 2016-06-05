a4fa326e18a90e7b3a32cddb600dcc709b432d2a
jaumeortola <jaumeortola@gmail.com>
2016-06-05 08:56:12 +0000

    write exceptinside one per line

--- no-exceptions-replace.sh
+++ no-exceptions-replace.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 language_dir=ca
 excepttitle=`cat $language_dir/excepttitle.cfg | tr '\n' '|' | sed -r 's/\|$//' `
-exceptinside=`cat $language_dir/exceptinside.cfg`
+exceptinside=`cat $language_dir/exceptinside.cfg  | tr '\n' '|' | sed -r 's/\|$//' `
 #echo python core/pwb.py replace.py "\b$1\b" "$2" -search:"\"$1\"" -regex -exceptitle:"$excepttitle" -exceptinside:"$exceptinside" -always -summary:"Corregit: $1 > $2"
 python ~/pywikibot-core/pwb.py replace -ns:0 -search:"\"$1\"" "\b$1\b" "$2" -regex -exceptinsidetag:hyperlink -always -summary:"Corregit: $1 > $2"
 
--- replaceall.sh
+++ replaceall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 language_dir=ca
 excepttitle=`cat $language_dir/excepttitle.cfg | tr '\n' '|' | sed -r 's/\|$//' `
-exceptinside=`cat $language_dir/exceptinside.cfg`
+exceptinside=`cat $language_dir/exceptinside.cfg  | tr '\n' '|' | sed -r 's/\|$//'`
 #echo python core/pwb.py replace.py "\b$1\b" "$2" -search:"\"$1\"" -regex -exceptitle:"$excepttitle" -exceptinside:"$exceptinside" -always -summary:"Corregit: $1 > $2"
 #python core/pwb.py replace -page:Usuari:Langtoolbot/proves "\b$1\b" "$2" -regex -excepttitle:"$excepttitle" -exceptinside:"$exceptinside" -exceptinsidetag:hyperlink -always -summary:"Corregit: $1 > $2" -ns:0  -search:"\"$1\""  -page:"Usuari:Langtoolbot/proves"
 if [ "$#" -eq 2 ]; then
