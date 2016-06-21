8e7407b3cf7da7eba149f4e66b08164f77f67d58
jaumeortola <jaumeortola@gmail.com>
2016-06-21 12:03:22 +0000

    update scripts

--- download-latest-dump.sh
+++ download-latest-dump.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-LANGUAGE=ca
+LANGUAGE=`cat language-code.cfg`
 wget http://dumps.wikimedia.org/${LANGUAGE}wiki/latest/${LANGUAGE}wiki-latest-pages-articles.xml.bz2
 bzip2 -dk ${LANGUAGE}wiki-latest-pages-articles.xml.bz2
+mkdir ${LANGUAGE}-dump-data
+mv ${LANGUAGE}wiki-latest-pages-articles.* ${LANGUAGE}-dump-data
